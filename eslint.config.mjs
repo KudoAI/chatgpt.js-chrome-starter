4a65bf364d2cac6ee5ff08c8d3b69308c510fde4
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-14 08:38:33 -0800

    Moved JS config `rules` to bottom for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,13 @@ import eslintPluginYml from 'eslint-plugin-yml'
 export default [
     {
         files: ['**/*.js', '**/*.mjs'],
+        languageOptions: {
+            ecmaVersion: 'latest', sourceType: 'script',
+            globals: {
+                ...globals.browser, ...globals.node,
+                chatgpt: 'readonly', chrome: 'readonly', GM_info: 'readonly'
+            }
+        },
         rules: {
             ...js.configs.recommended.rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
@@ -18,13 +25,6 @@ export default [
             'no-inner-declarations': 'off', // allow function declarations anywhere
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
             'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
-        },
-        languageOptions: {
-            ecmaVersion: 'latest', sourceType: 'script',
-            globals: {
-                ...globals.browser, ...globals.node,
-                chatgpt: 'readonly', chrome: 'readonly', GM_info: 'readonly'
-            }
         }
     },
     { files: ['**/*.mjs', '**/lib*/*.js'], languageOptions: { sourceType: 'module' }},
