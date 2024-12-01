ba7d7447047a9506d3c84c971a694a193073edd7
adamlui@protonmail.com <adamlui@protonmail.com>
2024-12-01 01:43:20 -0800

    Un-modulized components/icons.mjs for simplicity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.node, chatgpt: 'readonly', chrome: 'readonly', config: 'writable',
-                dom: 'readonly', settings: 'writable'
+                dom: 'readonly', icons: 'writable', settings: 'writable'
             }
         },
         plugins: { regexp, 'js-styles': stylisticJS },
