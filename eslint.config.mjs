e7d2b82b6537f01b74f990f38ca5b62c4dcf601f
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-30 17:22:59 -0800

    Added `config` + `settings` to JS globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,8 @@ export default [
         languageOptions: {
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
-                ...globals.browser, ...globals.node,
-                chatgpt: 'readonly', chrome: 'readonly', dom: 'readonly', GM_info: 'readonly'
+                ...globals.browser, ...globals.node, chatgpt: 'readonly', chrome: 'readonly', config: 'writable',
+                dom: 'readonly', GM_info: 'readonly', settings: 'writable'
             }
         },
         plugins: { regexp, 'js-styles': stylisticJS },
