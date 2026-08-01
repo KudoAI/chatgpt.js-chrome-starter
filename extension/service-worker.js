44293c088ef3d3b251035f1cf5ab49e8e2711df7
Adam Lui <adam@kudoai.com>
2026-08-01 00:16:22 -0700

    Split line up

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -30,12 +30,13 @@ chrome.runtime.onMessage.addListener(async ({ action }) => {
         const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
         const chatgptTab = new URL(activeTab.url).hostname == 'chatgpt.com' ? activeTab
                          : await chrome.tabs.create({ url: chatgptURL })
-        if (activeTab != chatgptTab) await new Promise(resolve => // after new tab loads
-            chrome.tabs.onUpdated.addListener(function loadedListener(tabId, { status }) {
-                if (tabId == chatgptTab.id && status == 'complete') {
-                    chrome.tabs.onUpdated.removeListener(loadedListener) ; setTimeout(resolve, 1500) }
-            })
-        )
+        if (activeTab != chatgptTab) 
+            await new Promise(resolve => // after new tab loads
+                chrome.tabs.onUpdated.addListener(function loadedListener(tabId, { status }) {
+                    if (tabId == chatgptTab.id && status == 'complete') {
+                        chrome.tabs.onUpdated.removeListener(loadedListener) ; setTimeout(resolve, 1500) }
+                })
+            )
         chrome.tabs.sendMessage(chatgptTab.id, { action: 'showAbout', source: 'service-worker.js' })
     }
 })
