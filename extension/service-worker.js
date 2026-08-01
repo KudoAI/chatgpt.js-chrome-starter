0eeb5b9743cd35724c16cc5b8647ce456cc6f500
Adam Lui <adam@kudoai.com>
2026-08-01 00:16:55 -0700

    Deleted trailing space

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -30,7 +30,7 @@ chrome.runtime.onMessage.addListener(async ({ action }) => {
         const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
         const chatgptTab = new URL(activeTab.url).hostname == 'chatgpt.com' ? activeTab
                          : await chrome.tabs.create({ url: chatgptURL })
-        if (activeTab != chatgptTab) 
+        if (activeTab != chatgptTab)
             await new Promise(resolve => // after new tab loads
                 chrome.tabs.onUpdated.addListener(function loadedListener(tabId, { status }) {
                     if (tabId == chatgptTab.id && status == 'complete') {
